f6cfbcd55ed8ab6948435824908ef0304fa1a546
radufurtuna <151870447+radufurtuna@users.noreply.github.com>
2025-09-18 12:06:40 +0300

    Environment setup & featured products (Store)

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,14 +1,23 @@
 import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-produnts";
+import ProductList from "@/components/product-list";
+
 export const revalidate = 0;
 
 const HomePage = async () => {
+  const products = await getProducts({isFeatured: true}); 
   const billboard = await getBillboard("64db85bf-c76c-4944-9e37-74fb5c56d949");
+  
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard}/>
+     
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products}/>
+      </div>
       </div>
     </Container>
   );
--- types.ts
+++ types.ts
@@ -8,3 +8,31 @@ id: string;
 name: string;
 billboard: Billboard;
 };
+
+export interface Product {
+id: string;
+name: string;
+price: string;
+category: Category;
+images: Image[];
+isFeatured: boolean;
+size: Size;
+color: Color;
+};
+
+export interface Image {
+id: string;
+url: string;
+}
+
+export interface Size {
+id: string;
+name: string;
+value: string;
+}
+
+export interface Color {
+id: string;
+name: string;
+value: string;
+}
